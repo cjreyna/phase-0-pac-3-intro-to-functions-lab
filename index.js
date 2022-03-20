11df08cca355d8117a9ac377f113664b096631f9
Charles <cjreyna5150@gmail.com>
2022-03-19 22:44:55 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+"Hello!" .toUpperCase(); // 'Hello!'
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+const string = "Hello!";
+console.log(string);
+console.log(string.toUpperCase());
+
+string === string.toUpperCase();
+
+function logshout (string) {
+    console.log(Hello.toUpperCase());
+    return string.toUpperCase
+}
+function logShout (Hello) {
+    console.log(Hello.toUpperCase( ));
+}
+function logWhisper (Hello) {
+    console.log(Hello.toLowerCase( ));
+}
+function sayHiToHeadphonedRoommate(string) {
+    var canthear = "I can't hear you!"
+    var canhear = "YES INDEED!"
+    var answer = "I would love to!"
+    var stanwer = "Let's have dinner together!"
+    if (string.toLowerCase(string) === string) {
+        return canthear
+    }
+    else if (string.toUpperCase(string) === string) {
+        return canhear
+    }
+    else if ("Let's have dinner together!" === string) {
+        return answer
+    }
+}
